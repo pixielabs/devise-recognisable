164ac6310a39414448d6539bc6a842853d9a7249
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2020-09-29 17:33:59 +0100

    the security level is configurable

--- app/lib/recogniser.rb
+++ app/lib/recogniser.rb
@@ -1,7 +1,14 @@
 class Recogniser
-  # Checks 
+  @@required_scores = {
+    relaxed: 0,
+    normal: 1,
+    strict: 2
+  }
+
+  # Checks the current request against the details from the previous sign in.
   def self.recognise?(request, previous_session)
     score = 0
+
     # Is the user's IP different to the last one?
     # Is it more than a certain distance from the last successful sign in?
     #
@@ -16,15 +23,11 @@ class Recogniser
     end
 
     # Is the user's User Agent is different to the previous signin?
-    if previous_session.user_agent == request.user_agent
-      score += 1
-    end
+    score += 1 if previous_session.user_agent == request.user_agent
 
     # Is the user's Accept header is different to the previous signin?
-    if previous_session.accept_header == request.headers["HTTP_ACCEPT"]
-      score += 1
-    end
+    score += 1 if previous_session.accept_header == request.headers["HTTP_ACCEPT"]
 
-    score > 2
+    score > @@required_scores[Devise.security_level]
   end
 end
