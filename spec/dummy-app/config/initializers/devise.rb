94cc9940abe5dde298ee972425e0ebcfcc2d74e4
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2020-10-02 12:46:19 +0100

    test for debug mode

--- spec/dummy-app/config/initializers/devise.rb
+++ spec/dummy-app/config/initializers/devise.rb
@@ -293,7 +293,6 @@ Devise.setup do |config|
 
   # ==> Configuration for :registerable
   config.security_level = :strict
-  config.debug_mode = true
 
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
