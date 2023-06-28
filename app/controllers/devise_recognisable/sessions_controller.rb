3977c9e5d686e09ddf07ac971fb2fa81cd36cb4f
Ola Jimoh <ojimoh21@live.co.uk>
2023-06-28 17:58:20 +0100

    Update how we send error messages
    This change allows for the users error logger to send error messages their logger of choice

--- app/controllers/devise_recognisable/sessions_controller.rb
+++ app/controllers/devise_recognisable/sessions_controller.rb
@@ -4,7 +4,7 @@ class DeviseRecognisable::SessionsController < Devise::SessionsController
   append_before_action :check_for_authentication_token, only: :new
   prepend_before_action :perform_recognition_check, only: :create
   append_after_action :store_recognisable_details, only: :create
- 
+
   def perform_recognition_check
     # Find the user
     self.resource = resource_class.find_by(email: params[resource_name][:email])
@@ -30,33 +30,35 @@ class DeviseRecognisable::SessionsController < Devise::SessionsController
       end
 
       # debug or info_only mode
+      # If there is no error_logger set up, we don't even want to attempt to send a message
+      return unless Devise.error_logger
+
       if (Devise.debug_mode || Devise.info_only) && (Rails.env.production? || Rails.env.staging?)
-        require 'rollbar'
-        Rollbar.debug(guard.failures, 'Unrecognised request')
+        Devise.error_logger.call(guard.failures, 'Unrecognised request')
       end
     end
   end
 
   # Check whether the user is attempting to sign in from a link in an email.
   def check_for_authentication_token
-    if params[:token].present?
-      token = AuthenticationToken.decode(params[:token])
+    return unless params[:token].present?
 
-      # Check the token hasn't expired.
-      return new_session_path(resource_class) if token[:exp] < Time.now.to_i
+    token = AuthenticationToken.decode(params[:token])
 
-      self.resource = resource_class.find token['id']
+    # Check the token hasn't expired.
+    return new_session_path(resource_class) if token[:exp] < Time.now.to_i
 
-      sign_in self.resource
-      DeviseRecognisable::RecognisableSession.create!(
-        recognisable: resource_class.find_by(email: self.resource.email),
-        sign_in_ip: request.location.ip,
-        sign_in_at: Time.now,
-        user_agent: request.user_agent,
-        accept_header: request.headers["HTTP_ACCEPT"]
-      )
-      redirect_to after_sign_in_path_for(resource)
-    end
+    self.resource = resource_class.find token['id']
+
+    sign_in self.resource
+    DeviseRecognisable::RecognisableSession.create!(
+      recognisable: resource_class.find_by(email: self.resource.email),
+      sign_in_ip: request.location.ip,
+      sign_in_at: Time.now,
+      user_agent: request.user_agent,
+      accept_header: request.headers["HTTP_ACCEPT"]
+    )
+    redirect_to after_sign_in_path_for(resource)
   end
 
   # After the user has been signed in, we save the ip address in the
