12b33e128715d1717c7dc0b544304d04872c7069
Max Prettyjohns <max@pixielabs.io>
2023-08-25 15:27:22 +0100

    cleanup tests

--- spec/dummy-app/spec/features/sign_up_spec.rb
+++ spec/dummy-app/spec/features/sign_up_spec.rb
@@ -4,20 +4,6 @@ RSpec.feature "Sign in" do
   let(:email) { Faker::Internet.email }
   let(:password) { Faker::Internet.password }
 
-  let!(:user_agent) { 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36' }
-  let!(:accept_header) { 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' }
-  # let!(:recognisable_session_values) {{
-  #   recognisable_id: user.id,
-  #   recognisable_type: 'User',
-  #   user_agent: user_agent,
-  #   accept_header: accept_header
-  # }}
-
-  before do
-    Capybara.current_session.driver.header('User-Agent', user_agent)
-    Capybara.current_session.driver.header('Accept', accept_header)
-  end
-
   context 'as a new user' do
     it 'creates a devise recognisable session' do
       visit '/'
